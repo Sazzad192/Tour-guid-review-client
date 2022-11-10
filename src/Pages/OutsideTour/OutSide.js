3c4eca715096c036583ad0474dceab9636a62434
Sazzad192 <sazadulbg130@gmail.com>
2022-11-10 22:23:26 +0600

    final commit

--- src/Pages/OutsideTour/OutSide.js
+++ src/Pages/OutsideTour/OutSide.js
@@ -8,12 +8,12 @@ import 'react-photo-view/dist/react-photo-view.css';
 
 const OutSide = () => {
     return (
-        <div className='mb-11'>
+        <div className='mb-11 '>
             <h2 className='text-4xl font-bold text-center'>My coverage spots</h2>
-            <h2 className='text-lg w-1/2 mx-auto text-center mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut placeat culpa quaerat dolor assumenda nihil vitae illo rerum, praesentium doloribus! Veritatis, sequi necessitatibus? Iste fuga, doloremque dolore voluptatem sit quaerat!</h2>
+            <h2 className='text-lg mx-auto text-center mt-3 sm:w-10/12 lg:w-1/2'>Out site of the hill tracking is. I was working as a tou guide at Cox-Bazar, Kashmit, kolkata, Ladakh, Sikim and many more.</h2>
 
-            <div className='grid w-11/12 mx-auto my-10 sm:grid-cols-1 lg:grid-cols-3 gap-4'>
-                <div className="card w-10/12 bg-base-300 text-primary shadow-xl">
+            <div className='grid w-11/12 mx-auto my-10 sm:grid-cols-1 lg:grid-cols-3 gap-7'>
+                <div className="card bg-base-300 text-primary shadow-xl">
                     <PhotoProvider>
                         <PhotoView src={cox}>
                             <figure><img src={cox} alt="cox" /></figure>
@@ -25,7 +25,7 @@ const OutSide = () => {
                     <p className='flex items-start'> <MdLocationPin className='text-xl'/> Bangladesh </p>
                     </div>
                 </div>
-                <div className="card w-10/12 bg-base-300 text-primary shadow-xl">
+                <div className="card bg-base-300 text-primary shadow-xl">
                     <PhotoProvider>
                         <PhotoView src={kashmir}>
                         <figure><img src={kashmir} alt="Shoes" /></figure>
@@ -36,7 +36,7 @@ const OutSide = () => {
                     <p className='flex items-start'> <MdLocationPin className='text-xl'/> India </p>
                     </div>
                 </div>
-                <div className="card w-10/12 bg-base-300 text-primary shadow-xl">
+                <div className="card bg-base-300 text-primary shadow-xl">
                     <PhotoProvider>
                         <PhotoView src={ladakh}>
                             <figure><img src={ladakh} alt="Shoes" /></figure>
--- src/Pages/Shared/Navbar/Nav.js
+++ src/Pages/Shared/Navbar/Nav.js
@@ -36,9 +36,9 @@ const Nav = () => {
                 <label tabIndex={0} className="btn btn-ghost lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
-                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    {navItems}
-                </ul>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {navItems}
+                    </ul>
                 </div>
                 <Link><img className='sm:w-16 h-16 ml-1 lg:w-20 h-20' src={logo} alt="" /></Link>
             </div>
